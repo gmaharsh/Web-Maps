77130b1c664ee2fa0ea1650be38eb51f0dfe2a41
gmaharsh <gmaharsh@yahoo.com>
2019-06-17 23:55:57 +0530

    Importing Markers from external sources

--- map1.py
+++ map1.py
@@ -1,11 +1,19 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+
+lat= list(data["LAT"])
+lon= list(data["LON"])
+
+
 map = folium.Map(location = [50, 100], zoom_start=6)
 
 fg = folium.FeatureGroup(name="My Map")
 
 
-for co in [[50.2, 99], [55, 85], [69.2, 79]]:
-    fg.add_child(folium.Marker(location=co,popup="Hello", icon=folium.Icon(color='green')))
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln],popup="Hello", icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 map.save("Map1.html")
