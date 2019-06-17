22cb8428a29046f059d736a007c602893c3c4178
gmaharsh <gmaharsh@yahoo.com>
2019-06-17 23:44:16 +0530

    Added Multiple Marker Options

--- map1.py
+++ map1.py
@@ -2,7 +2,10 @@ import folium
 map = folium.Map(location = [50, 100], zoom_start=6)
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location= [50.2, 99],popup="Hello", icon=folium.Icon(color='green')))
+
+
+for co in [[50.2, 99], [55, 85], [69.2, 79]]:
+    fg.add_child(folium.Marker(location=co,popup="Hello", icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 map.save("Map1.html")
