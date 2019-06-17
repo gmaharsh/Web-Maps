2dc47075f92b8e5138c35c6d2933d63b78845881
gmaharsh <gmaharsh@yahoo.com>
2019-06-18 00:22:12 +0530

    Added Circle Markers

--- map1.py
+++ map1.py
@@ -7,13 +7,23 @@ lat= list(data["LAT"])
 lon= list(data["LON"])
 elev = list(data["ELEV"])
 
+
+def color_pro(elev):
+    if(elev < 1500):
+        return 'green'
+    elif(1500 <= elev < 2200):
+        return 'blue'
+    else:
+        return 'red'
+
+
 map = folium.Map(location = [38, -100], zoom_start=6)
 
 fg = folium.FeatureGroup(name="My Map")
 
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln],popup=str(el) + "m", icon=folium.Icon(color='green')))
+    fg.add_child(folium.CircleMarker(location=[lt, ln],popup=str(el) + "m", radius = 6, fill_color = color_pro(el), color= 'grey', fill_opacity= 0.7))
 
 map.add_child(fg)
 map.save("Map1.html")
